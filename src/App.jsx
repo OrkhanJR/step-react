4f8cc1684eed25c577246012c00c187c8fafe34e
Мамедов Орхан Гасан <Mamed_bl70@itstep.edu.az>
2024-01-28 13:01:13 +0400

    tasks done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
-import Gallery from "./components/Gallery/Gallery"
+import Gallery from "./components/Gallery/Gallery";
+import { useState } from "react";
 
 const App = () => {
+  const [value, setValue] = useState(0);
+  const [step, setStep] = useState(1);
+
+  const increase = () => {
+    setValue(value + step);
+  };
+
+  const decrease = () => {
+    setValue(value - step);
+  };
+
+  const stepPlus = () => {
+    setStep(step + 1);
+  };
+
+  const stepMinus = () => {
+    if (step <= 1) return;
+    setStep(step - 1);
+  };
+
   return (
     <>
-    <Gallery />
+      <button onClick={stepMinus}>Minus step</button>
+      <button onClick={stepPlus}>Plus step</button>
+      <div>{step.toString()}</div>
+      <br />
+      <button onClick={decrease}>Minus</button>
+      <button onClick={increase}>Plus</button>
+      <div>{value.toString()}</div>
     </>
   );
-}
+};
 
 export default App;
